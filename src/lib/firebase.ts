1f7f50d18b0a3fc9a0c2044782b4c9385416b63f
kairo kairo <GMaina424@gmail.com>
2025-08-10 12:20:42 +0000

    this errors are happening after i deployed on vercel:Failed to load reso

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
+
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCMFg1ivJt_0Ueivfc1p41p2u9c9FVXjOI",
+  authDomain: "sira-pharmacy-online.firebaseapp.com",
+  projectId: "sira-pharmacy-online",
+  storageBucket: "sira-pharmacy-online.appspot.com",
+  messagingSenderId: "1072978083547",
+  appId: "1:1072978083547:web:8655883944a67f08d3e230"
 };
 
 // Initialize Firebase
